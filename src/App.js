cb27bb4ac74b7dfa1e07326860137ccaf64d5255
Ahn <asy3310@gmail.com>
2023-03-07 16:58:15 +0900

    노마드코더 ReactJS로 영화 웹 서비스 만들기 3장 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'; 
 
 function App() {
+  const [index, setIndex] = useState("xx")
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="xx">Select your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr />
+      {index === "xx" ? "plese select" : null}
+      {index === "0" ? <MinutesToHours/> : null}
+      {index === "1" ? <KmToMiles/> : null}
     </div>
   );
+}
+
+function KmToMiles(){
+  const [amount, setAmount] = useState()
+  const [inverted, setInverted] = useState(false);
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  }
+
+  const reset = () =>{setAmount(0)};
+  const onFlip = () => {
+    reset();
+    setInverted((currunt) => !currunt)
+  };
+  return (
+    <div>
+       <h3>KM 2 Miles</h3>
+       <div>
+        <label htmlFor="km">Km</label>
+        <input 
+          value={inverted ?  amount*1.609344 : amount} 
+          id="km" 
+          placeholder='Km'
+          type="Number"
+          onChange={onChange}
+          disabled={inverted}
+        />
+      </div>
+      <div>
+        <label htmlFor="miles">Miles</label>
+        <input 
+          value={inverted ? amount : amount/1.609344}
+          id="miles" 
+          placeholder='Miles' 
+          type="Number"
+          onChange={onChange}
+          disabled={!inverted}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>{inverted ? "Turn back" : "Invert"} </button>
+    </div>
+ )
+}
+function MinutesToHours () {
+  const [amount, setAmount] = useState()
+  const [inverted, setInverted] = useState(false);
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  }
+
+  const reset = () =>{setAmount(0)};
+  const onFlip = () => {
+    reset();
+    setInverted((currunt) => !currunt)
+  };
+  return (
+    <div>
+      <div>
+        <label htmlFor="minutes">Minutes</label>
+        <input 
+          value={inverted ?  amount*60 : amount}
+          id="minutes" 
+          placeholder='Minutes'
+          type="Number"
+          onChange={onChange}
+          disabled={inverted}
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours</label>
+        <input 
+          value={inverted ? amount : Math.round(amount/60)}
+          id="hours" 
+          placeholder='Hours' 
+          type="Number"
+          onChange={onChange}
+          disabled={!inverted}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>{inverted ? "Turn back" : "Invert"} </button>
+    </div>
+  )
 }
 
 export default App;
