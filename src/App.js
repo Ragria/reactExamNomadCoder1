86cce318cfd35aefa39ef6383bf1a3cb1127950f
Ahn <asy3310@gmail.com>
2023-03-08 10:44:23 +0900

    4장 끝

--- src/App.js
+++ src/App.js
@@ -1,110 +1,35 @@
 import './App.css';
+import logo from './logo.svg';
 import React, { useState } from 'react'; 
 
-function App() {
-  const [index, setIndex] = useState("xx")
-  const onSelect = (event) => {
-    setIndex(event.target.value);
-  }
+function Btn(props) {
+  return (
+    <button style={{
+      backgroundColor : "tomato",
+      color : "wheat",
+      padding : "10px 20px",
+      border : 0,
+      borderRadius : 10,
+      fontSize : props.fontSize
+    }}
+    onClick={props.changeValue}
+    >
+      {props.text}
+    </button>
+  )
+}
 
+const MemoriazeBtn =React.memo(Btn)
+
+function App() {
+  const [value, setValue] = useState("Save Change");
+  const changeValue = () => setValue("Revert Change");
   return (
     <div className="App">
-      <h1>Super Converter</h1>
-      <select value={index} onChange={onSelect}>
-        <option value="xx">Select your units</option>
-        <option value="0">Minutes & Hours</option>
-        <option value="1">Km & Miles</option>
-      </select>
-      <hr />
-      {index === "xx" ? "plese select" : null}
-      {index === "0" ? <MinutesToHours/> : null}
-      {index === "1" ? <KmToMiles/> : null}
+      <MemoriazeBtn text={value} changeValue={changeValue} />
+      <MemoriazeBtn text="Contines" fontSize={18}/>
     </div>
   );
-}
-
-function KmToMiles(){
-  const [amount, setAmount] = useState()
-  const [inverted, setInverted] = useState(false);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  }
-
-  const reset = () =>{setAmount(0)};
-  const onFlip = () => {
-    reset();
-    setInverted((currunt) => !currunt)
-  };
-  return (
-    <div>
-       <h3>KM 2 Miles</h3>
-       <div>
-        <label htmlFor="km">Km</label>
-        <input 
-          value={inverted ?  amount*1.609344 : amount} 
-          id="km" 
-          placeholder='Km'
-          type="Number"
-          onChange={onChange}
-          disabled={inverted}
-        />
-      </div>
-      <div>
-        <label htmlFor="miles">Miles</label>
-        <input 
-          value={inverted ? amount : amount/1.609344}
-          id="miles" 
-          placeholder='Miles' 
-          type="Number"
-          onChange={onChange}
-          disabled={!inverted}
-        />
-      </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={onFlip}>{inverted ? "Turn back" : "Invert"} </button>
-    </div>
- )
-}
-function MinutesToHours () {
-  const [amount, setAmount] = useState()
-  const [inverted, setInverted] = useState(false);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  }
-
-  const reset = () =>{setAmount(0)};
-  const onFlip = () => {
-    reset();
-    setInverted((currunt) => !currunt)
-  };
-  return (
-    <div>
-      <div>
-        <label htmlFor="minutes">Minutes</label>
-        <input 
-          value={inverted ?  amount*60 : amount}
-          id="minutes" 
-          placeholder='Minutes'
-          type="Number"
-          onChange={onChange}
-          disabled={inverted}
-        />
-      </div>
-      <div>
-        <label htmlFor="hours">Hours</label>
-        <input 
-          value={inverted ? amount : Math.round(amount/60)}
-          id="hours" 
-          placeholder='Hours' 
-          type="Number"
-          onChange={onChange}
-          disabled={!inverted}
-        />
-      </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={onFlip}>{inverted ? "Turn back" : "Invert"} </button>
-    </div>
-  )
 }
 
 export default App;
